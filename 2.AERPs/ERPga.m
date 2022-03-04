d3a0481b2d2de43d15b059957b7921738433b8cf
AlmaSTT <78170948+AlmaSTT@users.noreply.github.com>
2022-03-04 13:51:27 -0600

    Update ERPga.m

--- 2.AERPs/ERPga.m
+++ 2.AERPs/ERPga.m
@@ -1,7 +1,17 @@
 function GaERP=ERPga(ERP)
-[~,n,fs]=size(ERP);
-GaERP=zeros(n,fs);
+%%
+%Function to generate Grand-Average ERPs of a group of subjects per EEG channel
+% Input:  ERP   -   m x n x fs
+%            m  - number of ephocs to be averanged betwen subjects and channels
+%            n  - EEG channels
+%            s  - samples of individual epoch
+% Output: GaERP - n x s
+%            n  - EEG channels
+%            s - samples of individual epoch
+%by AlmaSTT
+[~,n,s]=size(ERP);
+GaERP=zeros(n,s);
 for i=1:n
-    GaERP(i,:)=reshape(mean(ERP(:,i,:),1),1,fs);
+    GaERP(i,:)=reshape(mean(ERP(:,i,:),1),1,s);
 end
 end
