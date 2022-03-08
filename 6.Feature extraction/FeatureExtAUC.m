062f3151c73ac77474f54b4307ae9a9ef92000ad
AlmaSTT <78170948+AlmaSTT@users.noreply.github.com>
2022-03-07 20:35:55 -0600

    Add files via upload

---
+++ 6.Feature extraction/FeatureExtAUC.m
@@ -0,0 +1,11 @@
+%Feature extraction per therapy
+%1) Calculate the AUC per therapy and initial and final sesion
+aucs1=ERPauc(ERPga1);
+aucs4=ERPauc(ERPga4);
+%2) Obtain the diference betwen initial - final sesion
+AUCd=abs(aucs1-aucs4);
+%3) Feature extraction of maximum value of AUC difference among channels 
+% and the corresponding channel 
+[AUCm,AUCch]=max(AUCd');
+AUCm=AUCm';
+AUCch=AUCch';
